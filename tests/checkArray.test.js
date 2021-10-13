e25eb365fc9f2809134457b58f56ab0ba5eb8273
Umar <umar.mahmood@infinityworks.com>
2021-10-13 16:05:13 +0100

    outline test cases

---
+++ tests/checkArray.test.js
@@ -0,0 +1,15 @@
+const { checkArray } = require("../src/app")
+
+describe("When we check an array for a winner", () => {
+    test("can find red won", () => {
+
+    })
+
+    test("can find yellow won", () => {
+
+    })
+
+    test("can recognise when there's a gap between 4 in a row", () => {
+        
+    })
+})

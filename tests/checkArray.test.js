b7830ae65ce2fbd5e54603ee72dbefeb13bb6774
Umar <umar.mahmood@infinityworks.com>
2021-10-13 16:31:58 +0100

    checkArray tests done

--- tests/checkArray.test.js
+++ tests/checkArray.test.js
@@ -3,13 +3,34 @@ const { checkArray } = require("../src/app")
 describe("When we check an array for a winner", () => {
     test("can find red won", () => {
 
+        const array = [null, null, "red", "red", "red", "red"]
+
+        const expected = "red"
+
+        const actual = checkArray(array)
+
+        expect(actual).toBe(expected)
     })
 
     test("can find yellow won", () => {
 
+        const array = [null, null, "yellow", "yellow", "yellow", "yellow"]
+
+        const expected = "yellow"
+
+        const actual = checkArray(array)
+
+        expect(actual).toBe(expected)
     })
 
     test("can recognise when there's a gap between 4 in a row", () => {
-        
+
+        const array = [null, "red", "red", null, "red", "red"]
+
+        const expected = null
+
+        const actual = checkArray(array)
+
+        expect(actual).toBe(expected)
     })
 })
